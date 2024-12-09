bf3921655cb6fd472101484b62fb5978c7b2fd12
ritik chauhan <hritiksanhals@gmail.com>
2024-12-09 20:38:42 +0530

    splash screen updates

--- pravaasa/src/components/splashScreen.js
+++ pravaasa/src/components/splashScreen.js
@@ -10,6 +10,7 @@ const SplashScreen = () => {
         width={400}
         height={400}
         priority
+        className="w-48 h-48 sm:w-48 sm:h-48 md:w-48 md:h-48 lg:w-64 lg:h-64 xl:w-80 xl:h-80"
       />
     </div>
   );
